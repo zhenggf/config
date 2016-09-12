e2ad12b1b2e48bcde35098d41c974ed8a6001edc
zhenggf <zheng.guifu@foxmail.com>
2016-09-12 11:04:40 +0800

    test

--- config/src/main/java/cn/orgid/funny/config/domain/component/AccessTokenThreadLocalComponent.java
+++ config/src/main/java/cn/orgid/funny/config/domain/component/AccessTokenThreadLocalComponent.java
@@ -7,6 +7,7 @@ import cn.orgid.funny.config.domain.model.app.AccessToken;
 @Component
 public class AccessTokenThreadLocalComponent {
 	
+	//test 测试
 	private ThreadLocal<AccessToken> local=new ThreadLocal<AccessToken>();
 	
 	public void set(AccessToken t){
